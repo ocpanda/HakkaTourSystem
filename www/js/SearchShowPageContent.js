a1995bddd0126bed8dccff27f1ccee409f4b089b
ocpanda <tma590g2t1r3@gmail.com>
2016-10-25 08:19:28 +0800

    fixed

---
+++ www/js/SearchShowPageContent.js
@@ -0,0 +1,46 @@
+var pages = ["#pageOne", "#pageTwo"];
+var nowPage = 0;
+alert("1");
+
+$(document).on("pageshow", "#search",function(){
+	pageTopBKColorInit();
+	pageRender();
+	$("#pageOne").show();
+});
+
+$("#goPageOne").on("click", function(){
+	nowPage = 0;
+	pageTopBKColorInit();
+	pageRender();
+	htmlShow();
+});
+
+$("#goPageTwo").on("click", function(){
+	nowPage = 1;
+	pageTopBKColorInit();
+	pageRender();
+	htmlShow();
+});
+
+function pageTopBKColorInit(){
+	$("#pageOne").hide();
+	$("#pageTwo").hide();
+	$("#goPageOne").css("background", "white");
+	$("#goPageTwo").css("background", "white");
+}
+
+function pageRender(){
+	switch(nowPage){
+		case 0:
+			$("#goPageOne").css("background", "#6666FF");
+			break;
+
+		case 1:
+			$("#goPageTwo").css("background", "#6666FF");
+			break;
+	}
+}
+
+function htmlShow(){
+	$(pages[nowPage]).show();
+}
